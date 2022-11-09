6ffb91fb8467f74dcc9b6cfde8c634682ebe83de
dzient <34776686+dzient@users.noreply.github.com>
2022-11-09 12:55:25 -0500

    Add files via upload

---
+++ Exercise1/KayneREST/Program.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------
+// David Zientara
+// 11-9-2022
+// Program prints Kayne + Ron Swanson quotes 
+// 
+//--------------------------------------------
+
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using KayneREST;
+
+namespace KayneWest
+{
+    class Program
+    {
+        private static string? kanyeURL = "https://api.kanye.rest";
+        private static string? ronSwansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+        public static void Main(string[] args) //, JObject jObject)
+        {
+            //Program just alternates between Kayne quotes (from the Kayne API)
+            //and Ron Swanson quotes (from the Ron Swanson quotes API) and prints them out
+            var client = new HttpClient();
+            int i = 0;
+            KayneREST.QuoteGenerator quote = new QuoteGenerator();
+
+            for (i = 0; i < 5; i++)
+            {
+                // Get and print a Kayne response:
+                try
+                {
+                    var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+                    var kanyeQuote = JObject.Parse(json: kanyeResponse).GetValue("quote").ToString();
+                    Console.WriteLine($"Kayne West: {quote.GetKayneQuote()}");
+                }
+                catch (Exception ex) // This is the generic catch-all exeception, included per your instructions
+                {
+                    Console.WriteLine("Unknown error: " + ex.Message);
+                }
+                // Do the same thing for Ron Swanson:
+                try
+                {
+                    var ronSwansonResponse = client.GetStringAsync(ronSwansonURL).Result;
+                    var ronSwansonQuote = JArray.Parse(ronSwansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                    Console.WriteLine($"Ron Swanson: {ronSwansonQuote}");
+                }
+                catch (Exception ex) // This is the generic catch-all exeception, included per your instructions
+                {
+                    Console.WriteLine("Unknown error: " + ex.Message);
+                }
+                // Print a separator to separate the quotes:
+                Console.WriteLine("========================");
+             }
+        }
+    }
+}
---
+++ Exercise1/KayneREST/QuoteGenerator.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KayneREST
+{
+    public class QuoteGenerator
+    {
+        private static string? ronSwansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+        int i = 0;
+
+        string kayneResponse, ronResponse, chuckResponse, musicResponse, numbersResponse;
+        private string? kayneURL = "https://api.kanye.rest";
+        private string chuckURL = "https://api.chucknorris.io/jokes/random";
+        private string musixmatchURL = "https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=it&f_has_lyrics=1"; //" https://api.musixmatch.com/ws/1.1/";
+        private string numbersURL = "http://numbersapi.com/#";
+        /// </summary>
+
+        public QuoteGenerator()
+        {
+            var client = new HttpClient();
+            kayneResponse = client.GetStringAsync(kayneURL).Result;
+            ronResponse = client.GetStringAsync(ronSwansonURL).Result;
+            //musicResponse = client.GetStringAsync(musicmatchURL).Result();
+
+        }
+
+        public String GetKayneQuote()
+        {
+            //var client = new HttpClient();
+            //var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            return JObject.Parse(json: kayneResponse).GetValue("quote").ToString();
+        }
+        public string getRonQuote()
+        {
+            //var ronSwansonResponse = client.GetStringAsync(ronSwansonURL).Result;
+            var ronSwansonQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            return ronSwansonQuote;
+        }
+        public string ChuckNorrisQuote()
+        {
+            var client = new HttpClient();
+            chuckResponse = client.GetStringAsync(chuckURL).Result;
+            if (chuckResponse != null)
+                return JObject.Parse(json: chuckResponse).GetValue("value").ToString();
+            return "Insert Chuck Norris quote here";
+        }
+        public string NumbersQuote()
+        {
+            var rand = new Random();
+            var client = new HttpClient();
+            int num = rand.Next() % 100;
+            string res = numbersURL + num.ToString();
+            numbersResponse = client.GetStringAsync(numbersURL).Result;
+            if (numbersResponse != null)
+                return JObject.Parse(json: chuckResponse).GetValue("text").ToString();
+            //return numbersResponse.ToString();
+             //   return JArray.Parse(json: numbersResponse).ToString();
+
+            return "Insert numbers quote here";
+        }
+    }
+}
